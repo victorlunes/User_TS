e11fa4453a7571feda76688c8be8f2ec3a42ef1f
Victor Lunes <victorllues@gmail.com>
2023-09-07 19:30:38 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello World")
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("heelo");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
